98bac7af76bb5d828fa603fc27035f9946419b7d
0lir055 <olirossxiv@gmail.com>
2024-04-14 14:40:09 +0100

    Update window size variables in game.py

--- game.py
+++ game.py
@@ -6,8 +6,8 @@ import random
 snake_speed = 15
 
 # Window size
-window_x = 720
-window_y = 480
+screen_width = 800
+screen_height = 600
 
 # defining colors
 black = pygame.Color(0, 0, 0)
@@ -21,7 +21,7 @@ pygame.init()
 
 # Initialise game window
 pygame.display.set_caption('Snake Game')
-game_window = pygame.display.set_mode((window_x, window_y))
+game_screen = pygame.display.set_mode((screen_width, screen_height))
 
 # FPS (frames per second) controller
 fps = pygame.time.Clock()
@@ -36,8 +36,8 @@ snake_body = [[100, 50],
 			[70, 50]
 			]
 # fruit position
-fruit_position = [random.randrange(1, (window_x//10)) * 10, 
-				random.randrange(1, (window_y//10)) * 10]
+fruit_position = [random.randrange(1, (screen_width//10)) * 10, 
+				random.randrange(1, (screen_height//10)) * 10]
 
 fruit_spawn = True
 
@@ -64,7 +64,7 @@ def show_score(choice, color, font, size):
 	score_rect = score_surface.get_rect()
 	
 	# displaying text
-	game_window.blit(score_surface, score_rect)
+	game_screen.blit(score_surface, score_rect)
 
 # game over function
 def game_over():
@@ -82,10 +82,10 @@ def game_over():
 	game_over_rect = game_over_surface.get_rect()
 	
 	# setting position of the text
-	game_over_rect.midtop = (window_x/2, window_y/4)
+	game_over_rect.midtop = (screen_width/2, screen_height/4)
 	
 	# blit will draw the text on screen
-	game_window.blit(game_over_surface, game_over_rect)
+	game_screen.blit(game_over_surface, game_over_rect)
 	pygame.display.flip()
 	
 	# after 2 seconds we will quit the program
@@ -146,22 +146,22 @@ while True:
 		snake_body.pop()
 		
 	if not fruit_spawn:
-		fruit_position = [random.randrange(1, (window_x//10)) * 10, 
-						random.randrange(1, (window_y//10)) * 10]
+		fruit_position = [random.randrange(1, (screen_width//10)) * 10, 
+						random.randrange(1, (screen_height//10)) * 10]
 		
 	fruit_spawn = True
-	game_window.fill(black)
+	game_screen.fill(black)
 	
 	for pos in snake_body:
-		pygame.draw.rect(game_window, green,
+		pygame.draw.rect(game_screen, green,
 						pygame.Rect(pos[0], pos[1], 10, 10))
-	pygame.draw.rect(game_window, white, pygame.Rect(
+	pygame.draw.rect(game_screen, white, pygame.Rect(
 		fruit_position[0], fruit_position[1], 10, 10))
 
 	# Game Over conditions
-	if snake_position[0] < 0 or snake_position[0] > window_x-10:
+	if snake_position[0] < 0 or snake_position[0] > screen_width-10:
 		game_over()
-	if snake_position[1] < 0 or snake_position[1] > window_y-10:
+	if snake_position[1] < 0 or snake_position[1] > screen_height-10:
 		game_over()
 
 	# Touching the snake body
